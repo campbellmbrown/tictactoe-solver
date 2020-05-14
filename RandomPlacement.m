07f7c547ffcdc0d7532a7fa3a437f1111d979651
Campbell Brown <campbellbrown093@gmail.com>
2020-05-14 18:09:34 +1200

    Plotting random placement results

--- RandomPlacement.m
+++ RandomPlacement.m
@@ -1,20 +1,25 @@
-function [winner] = RandomPlacement(n)
+function [winner] = RandomPlacement(n, startingPlayer)
 
+% Initialising board, starting player, etc.
 board = zeros(n, n);
-player = 'X';
+player = startingPlayer;
 winner = 0;
 gameFinished = false;
 
+% Run until the game is finished
 while (~gameFinished)
+        % Find indices of empty spaces
         [emptyR, emptyC] = find(board == 0);
+        % If there are no more spaces, it is a tie
         if size(emptyR, 1) == 0
             gameFinished = true;
         else
+            % Choose a random position and place the players symbol
             index = randi(size(emptyR, 1));
             board(emptyR(index), emptyC(index)) = player;
-            
             % Check to see if someone has won
             for i = 1:n
+               % If there is a column, row, or diagonal of the player
                if all(board(i,:) == player) || all(board(i,:) == player)...
                        || all(board(n:n-1:n^2-1) == player)...
                        || all(board(1:n+1:n^2) == player)
@@ -23,28 +28,12 @@ while (~gameFinished)
                end
             end
         end
-    
+    % Switch the player for the next turn
     if player == 'X'
         player = 'O';
     elseif player == 'O'
         player = 'X';
     end
 end
-
-% 
-% switch winner
-%     case 0
-%         disp('It is a tie!')
-%     case 'X'
-%         disp('The winner is X!')
-%     case 'O'
-%         disp('The winner is O!')
-% end
-% for i = 1:n
-%     for j = 1:n
-%         fprintf('%s', board(i, j));
-%     end
-%     fprintf('\n');
-% end
 
 end
--- RunningScript.m
+++ RunningScript.m
@@ -1,1 +1,16 @@
-winner = RandomPlacement(3);
+
+winner = zeros(1, 1000);
+
+for i = 1:10000
+   winner(i) = RandomPlacement(3, 'X');
+end
+
+xWins = sum(winner == 'X');
+oWins = sum(winner == 'O');
+ties = sum(winner == 0);
+
+figure;
+labels = {'X Wins','O Wins','Ties'};
+pie([xWins, oWins, ties])
+legend(labels)
+title('Wins of Random Placements in Tic-tac-toe (X Starting, n = 10,000)');
